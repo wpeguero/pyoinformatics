8a6d42eb4d7814020724a091238f8666b429862e
wpeguerorosario <wpeguerorosario@outlook.com>
2020-08-02 19:31:16 -0400

    Updated the library. Included a function that found the location of keywords in the gene bank.

--- genbank.py
+++ genbank.py
@@ -1,6 +1,7 @@
 """Reads the Gene databases and processes the raw data into useful information."""
 
 def parse_DNA(filename):
+    """Extracts the DNA sequence from .gbk files."""
     fp = file(filename)
     data = fp.read()
     fp.close()
@@ -11,3 +12,16 @@ def parse_DNA(filename):
     dna = ''
     for i in a:
         spl=i.split()
+        dna += ''.join(spl[1:])
+    return dna
+
+def find_keyword_locs(data, keyword='   CDS '):
+    """Find the number of occurences a keyword appears and extracts them."""
+    ngenes = data.count(keyword)
+    keylocs = []
+    k = 0
+    for i in range(ngenes):
+        t = data.find(keyword.k)
+        keylocs.append(t)
+        k = t + 10
+    return keylocs
